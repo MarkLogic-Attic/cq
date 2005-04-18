2e58ff66c3a1ee4e8130604c8044254570a93a1e
Mike <mike@marklogic.com>
2005-04-18 01:30:34 +0000

    update copyright dates
    git-svn-id: http://developer.marklogic.com/svn/cq/trunk@293 e04f4502-82db-0310-b1af-f799f365da79

--- cq.js
+++ cq.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2003, 2004 Mark Logic Corporation. All rights reserved.
+// Copyright (c) 2003-2005 Mark Logic Corporation. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

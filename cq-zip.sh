9d556f77af925230840431e56441f718fc7fd809
Mike <mike@marklogic.com>
2006-02-17 19:47:46 +0000

    fixed script
    git-svn-id: http://developer.marklogic.com/svn/cq/trunk@409 e04f4502-82db-0310-b1af-f799f365da79

--- cq-zip.sh
+++ cq-zip.sh
@@ -7,8 +7,8 @@ DIRNAME=`pwd`/$DIRNAME/..
 (
  cd $DIRNAME \
  && pwd \
- && rm -f cq.zip \
- && zip -9 releases/mark-logic-cq-3.0.0.zip \
+ && rm -f releases/cq.zip \
+ && zip -9 releases/cq.zip \
    cq/cq.xqy cq/cq-[eq]*.xqy cq/cq.js cq/cq.css \
    cq/?arr.gif cq/worksheet.xml \
    cq/index.html cq/lib-*.xqy cq/cq-result.html cq/*.txt
